ac2bc519c51e61f8261227106027d93718e75e91
Nininanou16 <paul.peronredon@skypvp.fr>
2023-08-29 15:40:39 +0200

    add permission to command handler

--- Commands/test.js
+++ Commands/test.js
@@ -1,5 +1,6 @@
 module.exports = {
     modOnly: false,
+    subOnly: false,
     run: (Client, channel, tags, message, self) => {
         Client.say(channel, "Test OK")
     }
--- index.js
+++ index.js
@@ -15,13 +15,17 @@ client.connect().catch((error) => {
     console.log(error);
 });
 
-
 client.on('message', (channel, tags, message, self) => {
-    if (message.startsWith('!')) {
-        let args = message.split(/^\s+$/);
+    // if (self) return;
+
+    console.log(tags)
+    if (message.toLowerCase().startsWith('!')) {
+        let args = message.toLowerCase().split(/^\s+$/);
         let cmdName = args[0];
         let command = commands.get(cmdName);
         if (command) {
+            if (command.modOnly && (!tags.mod || !tags.badges.broadcaster == 1)) return;
+            if (command.subOnly && !tags.subscriber) return;
             command.run(client, channel, tags, message, self)
         }
     }
