b87bcd4dece545adadc43bffdd080912aa594c7c
Nininanou16 <paul.peronredon@skypvp.fr>
2023-08-29 15:41:13 +0200

    add cooldown system to commands

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const client = new Client({
     },
     channels: ['nininanou16']
 });
+console.log(client.timeout)
 const commands = new Map();
+// lastUse<map> { command: { general: XXX, }}
+let lastUse = new Map();
 require('./handler.js')(commands)
 
 client.connect().catch((error) => {
@@ -18,15 +21,32 @@ client.connect().catch((error) => {
 client.on('message', (channel, tags, message, self) => {
     // if (self) return;
 
-    console.log(tags)
+    // console.log(tags)
     if (message.toLowerCase().startsWith('!')) {
         let args = message.toLowerCase().split(/^\s+$/);
         let cmdName = args[0];
         let command = commands.get(cmdName);
         if (command) {
-            if (command.modOnly && (!tags.mod || !tags.badges.broadcaster == 1)) return;
-            if (command.subOnly && !tags.subscriber) return;
-            command.run(client, channel, tags, message, self)
+            // check permission
+            if (command.modOnly && (!tags.mod && tags.badges?.broadcaster != 1)) return console.log('utilisateur non modérateur');
+            if (command.subOnly && !tags.subscriber) return console.log('utilisateur non abonné');
+
+            // check cooldown
+            let cooldown = lastUse.get(cmdName);
+            if (!cooldown) {
+                lastUse.set(cmdName, {});
+                cooldown = lastUse.get(cmdName)
+            }
+            console.log(cooldown)
+            if (command.cooldown?.user > 0 && command.cooldown?.general > 0) {
+                    if ((Date.now() - cooldown[tags.username]) < (command.cooldown.user*1000)) return console.log('Cooldown utilisateur non écoulé');
+                    if ((Date.now() - cooldown.general) < (command.cooldown.general*1000)) return console.log('Cooldown general non écoulé');
+            }
+            command.run(client, channel, tags, message, self);
+            if (command.cooldown?.user > 0 && command.cooldown?.general > 0) {
+                cooldown.general = Date.now();
+                cooldown[tags.username] = Date.now()
+            }
         }
     }
 });
