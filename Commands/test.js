649f5751d3b89900de4b0d99312a8658cdc95a70
Nininanou16 <paul.peronredon@skypvp.fr>
2023-08-29 15:41:31 +0200

    add cooldown to test.js

--- Commands/test.js
+++ Commands/test.js
@@ -1,6 +1,10 @@
 module.exports = {
     modOnly: false,
     subOnly: false,
+    cooldown: {
+        user: 20,
+        general: 10
+    },
     run: (Client, channel, tags, message, self) => {
         Client.say(channel, "Test OK")
     }
