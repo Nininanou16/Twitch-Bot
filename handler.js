fe7225237c883917aa42bd452e4d979ce4e3f8b2
Nininanou16 <paul.peronredon@skypvp.fr>
2023-09-06 18:59:01 +0200

    add discord command handler and event handler

--- handler.js
+++ handler.js
@@ -1,13 +1,23 @@
 const {readdir} = require('node:fs/promises')
 
-module.exports = async (twitchCommands) => {
+module.exports = async (twitchCommands, discordCommands, discordClient) => {
     try {
-        const commandList = await readdir('./TwitchCommands')
-        for (const command of commandList) {
+        const twitchCommandList = await readdir('./TwitchCommands')
+        for (const command of twitchCommandList) {
             if (command.toString().endsWith('.js')) {
                 let cmdInfo = require(`./TwitchCommands/${command}`)
                 if (cmdInfo) {
                     twitchCommands.set(`!${command.split('.')[0]}`, cmdInfo)
+                }
+            }
+        }
+        
+        const discordCommandList = await readdir('./DiscordCommands')
+        for (const command of discordCommandList) { 
+            if (command.toString().endsWith('.js')) {
+                let cmdInfo = require(`./DiscordCommands/${command}`)
+                if (cmdInfo) {
+                    discordCommands.set(`${command.split('.')[0]}`, cmdInfo)
                 }
             }
         }
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
-const settings = require('./settings.json')
-const {twitchClient} = require('tmi.js');
-const twitchClient = new twitchClient({
+const { readdirSync } = require('node:fs');
+const settings = require('./settings.json');
+
+// Initiate Twitch IRC Client
+const tmi = require('tmi.js');
+const twitchClient = new tmi.Client({
     options: { debug: true },
     identity: {
         username: process.env.TWITCH_USERNAME,
@@ -9,9 +12,26 @@ const twitchClient = new twitchClient({
     },
     channels: settings.channels
 });
+
+const Discord = require('discord.js');
+const discordClient = new Discord.Client({ intents: 100143 });
+discordClient.login(process.env.DISCORD_TOKEN);
+        
+const discordEventList = readdirSync('./DiscordEvents')
+for (const eventName of discordEventList) { 
+    if (eventName.toString().endsWith('.js')) {
+        console.log(`Loading ${eventName} event...`)
+        const event = require(`./DiscordEvents/${eventName}`)
+        discordClient.on(eventName.split('.')[0], event.bind(null, discordClient))
+    }
+}
+
+// Initiate Twitch and Discord command handler
 const twitchCommands = new Map();
+const discordCommands = new Map();
+const discordEvents = new Map();
 let lastUse = new Map();
-require('./handler.js')(twitchCommands)
+require('./handler.js')(twitchCommands, discordCommands, discordClient);
 
 twitchClient.connect().catch((error) => {
     console.log(error);
@@ -24,12 +44,11 @@ setInterval(() => {
 
 twitchClient.on('message', (channel, tags, message, self) => {
     // if (self) return;
-    console.log(channel)
 
     if (message.toLowerCase().startsWith('!')) {
         let args = message.toLowerCase().split(/^\s+$/);
         let cmdName = args[0];
-        let command = commands.get(cmdName);
+        let command = twitchCommands.get(cmdName);
         if (command) {
             // check permission
             if (command.modOnly && (!tags.mod && tags.badges?.broadcaster != 1)) return;
